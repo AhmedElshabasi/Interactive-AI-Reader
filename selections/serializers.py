c81205aeb964d7722dcdc470d115962b8478651b
Ahmed Elshabasi <ahmed.elshabasi@ucalgary.ca>
2025-07-05 14:43:30 +0300

    PDF reader initialized. features: - Custom pdfs allowed. - Text selection enabled

--- selections/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Selection
-
-class SelectionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Selection
-        fields = '__all__' 
